645f67e04c49588dbaa6f9ea7ced5dddd4f66fab
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-13 12:53:02 +0530

    imported BrowserRouter and Router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { BrowserRouter} from "react-router-dom";
+
 const App = () => {
   return (
-    <h1>Welcome to React Router</h1>
+    <BrowserRouter>
+      {/* <Route to="/" component={Home}></Route> */}
+      {/* <Route to="/about" component={About}></Route> */}
+      {/* <Route to="/about" component={Portfolio}></Route> */}
+      {/* <Route to="/about" component={Contact}></Route> */}
+    </BrowserRouter>
   )
 }
 
