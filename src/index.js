43c288c181cc458557e1f3997d2c38cf3bf01e4a
Yugesh Palvai <yugeshpalvai21@gmail.com>
2022-04-13 13:32:36 +0530

    Implements Home Component renders on root path

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom';
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome To Portfolio App</h1>
+      <p>website to maintain portfolio</p>
+    </div>
+  )
+}
+
 const router = (
   <BrowserRouter>
-      <Route path="/" component={App} />
+      <Route path="/" component={Home} />
   </BrowserRouter>
 )
 
